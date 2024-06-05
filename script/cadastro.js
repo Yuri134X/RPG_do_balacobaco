1fabab7e7981a3785077b054fd8bfd22e1e5fae9
victorfake <victorhuebr173@gmail.com>
2024-06-05 13:17:07 -0300

    começo api, ainda falata testar com o banco

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,14 +1,31 @@
-const form = document.getElementById('form');
-        const errorMessage = document.getElementById('error-message');
+document.getElementById('register-form').addEventListener('submit', async function (e) {
+    e.preventDefault();
 
-        form.addEventListener('submit', (event) => {
-            event.preventDefault();
-            const password = document.getElementById('password').value;
-            const confirmPassword = document.getElementById('confirm-password').value;
-            if (password !== confirmPassword) {
-                errorMessage.textContent = 'As senhas não são iguais.';
-                errorMessage.style.display = 'block';
-            } else {
-                alert('Formulário enviado com sucesso!');
-            }
-        });
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const characterName = document.getElementById('character-name').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+
+    const response = await fetch('/api/users/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name,
+            email,
+            characterName,
+            password,
+            confirmPassword
+        })
+    });
+
+    const result = await response.json();
+
+    if (response.ok) {
+        alert('Usuário cadastrado com sucesso!');
+    } else {
+        alert('Erro: ' + result.error);
+    }
+});
---
+++ server/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/users');
+
+const app = express();
+const PORT = 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+app.use('/api/users', userRoutes);
+
+app.listen(PORT, () => {
+    console.log(`O Servidor esta rodando na porta http://localhost:${PORT}`);
+});
