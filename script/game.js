17ed2bffc92b13e8a4d08babb4e942e6eaa231cc
yuri134x <yurifilipe2004@gmail.com>
2024-06-06 09:49:25 -0300

    introdução

---
+++ script/game.js
@@ -0,0 +1,18 @@
+let paragraphs = document.querySelectorAll('.typing-text p');
+let continuarButton = document.getElementById('continuar');
+let iniciarJogoButton = document.getElementById('iniciar-jogo');
+let currentParagraph = 0;
+
+continuarButton.addEventListener('click', function() {
+  paragraphs[currentParagraph].classList.remove('active');
+  currentParagraph++;
+  paragraphs[currentParagraph].classList.add('active');
+  if (currentParagraph === paragraphs.length - 1) {
+    continuarButton.style.display = 'none';
+    iniciarJogoButton.style.display = 'block';
+  }
+});
+
+iniciarJogoButton.addEventListener('click', function() {
+  window.location.href = 'jogo.html';
+});
