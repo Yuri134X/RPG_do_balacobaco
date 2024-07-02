bc35d6071ab3b8863e0b3bef2747a4ecf4ee8f35
victorfake <victorhuebr173@gmail.com>
2024-07-02 17:57:11 -0300

    nomePersonagem Route1

--- historia/Scripts/capitulo1.js
+++ historia/Scripts/capitulo1.js
@@ -1,9 +1,18 @@
 const resume = document.getElementById('resume');
+const route1 = document.getElementById('route1');
+const route2 = document.getElementById('route2');
+const route3 = document.getElementById('route3');
 
 const nomeSalvo = localStorage.getItem('nome');
 
 resume.textContent = `${nomeSalvo} está determinado a buscar vingança contra Lunaro. Ele deve escolher como se preparar para essa jornada perigosa.`;
-//José está determinado a buscar vingança contra Lunaro. Ele deve escolher como se preparar para essa jornada perigosa.
+
+route1.textContent = `${nomeSalvo} decide ir para uma flortesta proxima em busca de magia sombria. Ele acredita que o poder sombrio pode ser a chave para derrotar Lunaro.`
+
+route2.textContent = `${nomeSalvo} decide buscar a orientação de Elazar, um velho amigo e mentor que vive nas montanhas. Elazar pode ajudá-lo a fortalecer suas habilidades arcanas de forma ética.`
+
+route3.textContent = `${nomeSalvo} resolve buscar aliados entre as criaturas mágicas das florestas de Sombra Verde. Ele espera que alguma esteja disposta a ajudá-lo em sua busca por vingança.`
+
 document.getElementById("path-1").addEventListener("click", function() {
     window.location.href = "capitulo2A.html";
 });
@@ -13,3 +22,4 @@ document.getElementById("path-2").addEventListener("click", function() {
 document.getElementById("path-3").addEventListener("click", function() {
     window.location.href = "capitulo2C.html";
 });
+//José resolve buscar aliados entre as criaturas mágicas das florestas de Sombra Verde. Ele espera que alguma esteja disposta a ajudá-lo em sua busca por vingança.
