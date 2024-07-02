a77107d21afe855a579011c844beb439c4d9770e
victorfake <victorhuebr173@gmail.com>
2024-07-02 10:53:40 -0300

    inicio de nomePersonagem usuario

--- script/game.js
+++ script/game.js
@@ -1,24 +1,27 @@
-const nomeImpresso = document.getElementById('nome-impresso');
-const textoComNome = document.getElementById('texto-com-nome');
+// game.html
+const texto1 = document.getElementById('texto1');
+const texto2 = document.getElementById('texto2');
+const texto3 = document.getElementById('texto3');
+const botao = document.getElementById('botao');
+const startButton = document.getElementById('start');
+
 const nomeSalvo = localStorage.getItem('nome');
 
-nomeImpresso.textContent = `${nomeSalvo}, um jovem mago, vivia em uma vila pacífica chamada Sombra Verde. Seus dias eram preenchidos com o estudo das artes arcanas e a cura de seus vizinhos com ervas mágicas.`;
+texto1.textContent = `${nomeSalvo}, um jovem mago, vivia em uma vila pacífica chamada Sombra Verde. Seus dias eram preenchidos com o estudo das artes arcanas e a cura de seus vizinhos com ervas mágicas.`;
 
-const texto = `${nomeSalvo} testemunhou a crueldade de Lunaro com seus próprios olhos. O mago perverso lançou uma maldição sobre a família de José. José viu sua esposa e seus filhos mortos. A dor e a raiva queimaram em seu coração, fazendo ele partir em uma jornada perigosa e cheia de escolhas.`;
-textoComNome.textContent = texto;
+texto2.textContent = `Um dia, porém, a tranquilidade de Sombra Verde foi quebrada. Um mago perverso chamado Lunaro, um feiticeiro habilidoso, mas sua magia era escura e cruel.`;
 
-document.getElementById('botao').addEventListener('click', function() {
-  var texto1 = document.getElementById('nome-impresso');
-  var texto2 = document.getElementById('texto2');
-  var texto3 = document.getElementById('texto3');
-  
+texto3.textContent = `${nomeSalvo} testemunhou a crueldade de Lunaro com seus próprios olhos. O mago perverso lançou uma maldição sobre a família de ${nomeSalvo}. ${nomeSalvo} viu sua esposa e seus filhos mortos. A dor e a raiva queimaram em seu coração, fazendo ele partir em uma jornada perigosa e cheia de escolhas.`;
+
+botao.addEventListener('click', function() {
   texto1.style.display = 'none';
   texto2.style.display = 'block';
+  
   setTimeout(function() {
     texto2.style.display = 'none';
     texto3.style.display = 'block';
   }, 5000);
-
+  
   setTimeout(function() {
     window.location.href = './historia/capitulo1.html';
   }, 15000);
