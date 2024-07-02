a36408cb532d06295f03c95b8a4556afa366a984
victorfake <victorhuebr173@gmail.com>
2024-07-02 20:16:45 -0300

    NomePersonagem cap 2B

--- historia/Scripts/capitulo2B.js
+++ historia/Scripts/capitulo2B.js
@@ -1,3 +1,18 @@
+const resume = document.getElementById('resume');
+const route1 = document.getElementById('route1');
+const route2 = document.getElementById('route2');
+const route3 = document.getElementById('route3');
+
+const nomeSalvo = localStorage.getItem('nome');
+
+resume.textContent = `${nomeSalvo} viaja para as montanhas e encontra Elazar, que concorda em ajudá-lo a aprimorar suas habilidades arcanas.`;
+
+route1.textContent = `${nomeSalvo} dedica-se completamente ao treinamento intensivo com Elazar, aprimorando suas habilidades e aprendendo novas magias.`
+
+route2.textContent = `${nomeSalvo} opta por explorar formas de magia menos convencionais, buscando um método inovador para enfrentar Lunaro.`
+
+route3.textContent = `Deixar o treinamento de Elazar e formar alianças com criaturas mágicas`
+
 document.getElementById("path-1").addEventListener("click", function() {
     window.location.href = "capitulo3C.html";
 });
