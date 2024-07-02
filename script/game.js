da9b62c4cf314de135a1ac52cf08ea0adc185d23
victorfake <victorhuebr173@gmail.com>
2024-07-02 10:04:49 -0300

    nome register

--- script/game.js
+++ script/game.js
@@ -1,9 +1,18 @@
+const nomeImpresso = document.getElementById('nome-impresso');
+const textoComNome = document.getElementById('texto-com-nome');
+const nomeSalvo = localStorage.getItem('nome');
+
+nomeImpresso.textContent = `${nomeSalvo}, um jovem mago, vivia em uma vila pacífica chamada Sombra Verde. Seus dias eram preenchidos com o estudo das artes arcanas e a cura de seus vizinhos com ervas mágicas.`;
+
+const texto = `${nomeSalvo} testemunhou a crueldade de Lunaro com seus próprios olhos. O mago perverso lançou uma maldição sobre a família de José. José viu sua esposa e seus filhos mortos. A dor e a raiva queimaram em seu coração, fazendo ele partir em uma jornada perigosa e cheia de escolhas.`;
+textoComNome.textContent = texto;
+
 document.getElementById('botao').addEventListener('click', function() {
-  var texto1 = document.getElementById('texto1');
+  var nomeImpresso = document.getElementById('texto1');
   var texto2 = document.getElementById('texto2');
   var texto3 = document.getElementById('texto3');
   
-  texto1.style.display = 'none';
+  nomeImpresso.style.display = 'none';
   texto2.style.display = 'block';
   setTimeout(function() {
     texto2.style.display = 'none';
--- script/script.js
+++ script/script.js
@@ -1,35 +1,43 @@
-const startButton = document.getElementById('start-button');
-const formContainer = document.getElementById('form-container');
-const loginButton = document.getElementById('login-button');
-const signupButton = document.getElementById('signup-button');
-const noSignupButton = document.getElementById('no-signup-button');
+//const startButton = document.getElementById('start-button');
+//const formContainer = document.getElementById('form-container');
+//const loginButton = document.getElementById('login-button');
+//const signupButton = document.getElementById('signup-button');
+//const noSignupButton = document.getElementById('no-signup-button');
+const form = document.getElementById('form-nome');
 
-startButton.addEventListener('click', () => {
-  startButton.style.display = 'none';
-  formContainer.style.display = 'block';
-  loginButton.style.display = 'block';
-  signupButton.style.display = 'block';
+form.addEventListener('submit', (e) => {
+ e.preventDefault();
+ const nome = document.getElementById('nome').value;
+ localStorage.setItem('nome', nome);
+ window.location.href = './game.html';
 });
 
-loginButton.addEventListener('click', () => {
-  signupButton.style.display = 'none';
-  loginForm.style.display = 'block';
-});
+//startButton.addEventListener('click', () => {
+//  startButton.style.display = 'none';
+//  formContainer.style.display = 'block';
+//  loginButton.style.display = 'block';
+//  signupButton.style.display = 'block';
+//});
 
-signupButton.addEventListener('click', () => {
-  loginButton.style.display = 'none';
-  loginForm.style.display = 'none';
-  signupForm.style.display = 'block';
-});
+//loginButton.addEventListener('click', () => {
+//  signupButton.style.display = 'none';
+//  loginForm.style.display = 'block';
+//});
 
-loginButton.addEventListener('click', () => {
-    window.location.href = './login.html';
-});
+//signupButton.addEventListener('click', () => {
+//  loginButton.style.display = 'none';
+//  loginForm.style.display = 'none';
+//  signupForm.style.display = 'block';
+//});
 
-signupButton.addEventListener('click', () => {
-    window.location.href = './signup.html';
-});
+//loginButton.addEventListener('click', () => {
+//    window.location.href = './login.html';
+//});
 
-noSignupButton.addEventListener('click', () => {
-  window.location.href = './game.html';
-});
+//signupButton.addEventListener('click', () => {
+//    window.location.href = './signup.html';
+//});
+
+//noSignupButton.addEventListener('click', () => {
+//  window.location.href = './game.html';
+//});
