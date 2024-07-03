0c31a3f2db25ab452d863726f3d5b732e1cdb130
victorfake <victorhuebr173@gmail.com>
2024-07-03 16:20:22 -0300

    novo cap2

--- historia/Scripts/capitulo2A.js
+++ historia/Scripts/capitulo2A.js
@@ -1,17 +1,7 @@
 const resume = document.getElementById('resume');
-const route1 = document.getElementById('route1');
-const route2 = document.getElementById('route2');
-const route3 = document.getElementById('route3');
-
 const nomeSalvo = localStorage.getItem('nome');
 
-resume.textContent = `${nomeSalvo} está na floresta, e encontra um tomo arcano entre as Raizes de uma antiga arvore. Ele sente a magia sombria começando a tomar conta de sua alma.`;
-
-route1.textContent = `${nomeSalvo} decide explorar todo o potencial da magia sombria, apesar dos riscos para sua alma.`
-
-route2.textContent = `${nomeSalvo} tenta encontrar um equilíbrio entre a magia sombria e suas próprias habilidades naturais para evitar ser consumido por ela.`
-
-route3.textContent = `${nomeSalvo} percebe os perigos da magia sombria e decide procurar ajuda com as criaturas mágicas na floresta.`
+resume.textContent = `${nomeSalvo}, adentrando as profundezas da ancestral floresta, se depara com um tomo arcano oculto entre as serpenteantes raízes de uma venerável árvore. Ao erguer o misterioso volume, uma aura de poder sombrio emana de suas páginas amareladas, envolvendo-o como um manto de trevas. Ele sente a magia negra lentamente permeando sua alma, insidiosa e irresistível, prometendo-lhe forças além da compreensão mortal. Em meio ao silêncio sepulcral da floresta, ${nomeSalvo} percebe que sua essência está sendo inexoravelmente moldada pela presença arcana, selando um pacto com as sombras na busca pela vingança contra Lunaro.`;
 
 document.getElementById("path-1").addEventListener("click", function() {
     window.location.href = "capitulo3A.html";
