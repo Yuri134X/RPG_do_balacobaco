dac2311207a60dc74fb073648bb2dd0757e43910
victorfake <victorhuebr173@gmail.com>
2024-07-03 16:07:43 -0300

    simplificação do Script

--- script/script.js
+++ script/script.js
@@ -1,8 +1,3 @@
-//const startButton = document.getElementById('start-button');
-//const formContainer = document.getElementById('form-container');
-//const loginButton = document.getElementById('login-button');
-//const signupButton = document.getElementById('signup-button');
-//const noSignupButton = document.getElementById('no-signup-button');
 const form = document.getElementById('form-nome');
 const nomeInput = document.getElementById('nome');
 
@@ -15,39 +10,6 @@ form.addEventListener('submit', (e) => {
   } else {
     alert('Por favor, digite seu nome de personagem.'); 
   }
-});
-
-//startButton.addEventListener('click', () => {
-//  startButton.style.display = 'none';
-//  formContainer.style.display = 'block';
-//  loginButton.style.display = 'block';
-//  signupButton.style.display = 'block';
-//});
-
-//loginButton.addEventListener('click', () => {
-//  signupButton.style.display = 'none';
-//  loginForm.style.display = 'block';
-//});
-
-//signupButton.addEventListener('click', () => {
-//  loginButton.style.display = 'none';
-//  loginForm.style.display = 'none';
-//  signupForm.style.display = 'block';
-//});
-
-//loginButton.addEventListener('click', () => {
-//    window.location.href = './login.html';
-//});
-//signupButton.addEventListener('click', () => {
-//    window.location.href = './signup.html';
-//});
-
-//noSignupButton.addEventListener('click', () => {
-//  window.location.href = './game.html';
-//});
-
-noSignupButton.addEventListener('click', () => {
-  window.location.href = './game.html';
 });
 
 function playMusic() {
