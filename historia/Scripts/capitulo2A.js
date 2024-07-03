9278c860e63e1eb2c6f525034b97b7285686390c
victorfake <victorhuebr173@gmail.com>
2024-07-03 16:26:29 -0300

    novo cap 2A

--- historia/Scripts/capitulo2A.js
+++ historia/Scripts/capitulo2A.js
@@ -1,7 +1,8 @@
 const resume = document.getElementById('resume');
 const nomeSalvo = localStorage.getItem('nome');
 
-resume.textContent = `${nomeSalvo}, adentrando as profundezas da ancestral floresta, se depara com um tomo arcano oculto entre as serpenteantes raízes de uma venerável árvore. Ao erguer o misterioso volume, uma aura de poder sombrio emana de suas páginas amareladas, envolvendo-o como um manto de trevas. Ele sente a magia negra lentamente permeando sua alma, insidiosa e irresistível, prometendo-lhe forças além da compreensão mortal. Em meio ao silêncio sepulcral da floresta, ${nomeSalvo} percebe que sua essência está sendo inexoravelmente moldada pela presença arcana, selando um pacto com as sombras na busca pela vingança contra Lunaro.`;
+resume.innerHTML = `${nomeSalvo}, adentrando as profundezas da ancestral floresta, se depara com um tomo arcano oculto entre as serpenteantes raízes de uma venerável árvore. Ao erguer o misterioso volume, uma aura de poder sombrio emana de suas páginas amareladas, envolvendo-o como um manto de trevas. Ele sente a magia negra lentamente permeando sua alma, insidiosa e irresistível, prometendo-lhe forças além da compreensão mortal. <br>
+Em meio ao silêncio sepulcral da floresta, ${nomeSalvo} percebe que sua essência está sendo inexoravelmente moldada pela presença arcana. Agora, ele deve decidir se abraçará o poder das trevas ou buscará um meio de resistir à sua sedução, pois seu destino, e a vingança contra Lunaro, dependem de sua próxima escolha.`;
 
 document.getElementById("path-1").addEventListener("click", function() {
     window.location.href = "capitulo3A.html";
